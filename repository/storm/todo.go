4af5c2a28dca24f4e2e9e828c3c4b96fdb686dcf
liyang <ismeade99@sina.com>
2021-08-31 23:19:44 +0800

    up

--- repository/storm/todo.go
+++ repository/storm/todo.go
@@ -2,29 +2,31 @@ package storm
 
 import (
 	"encoding/json"
-	"go-todolist/repository"
+	"fmt"
+	"go-todolist/model"
 	"time"
 
 	"github.com/asdine/storm/v3"
 )
 
-type todoRepository struct {
+type TodoRepository struct {
 	DB *storm.DB
 }
 
-func NewTodoRepository(db *storm.DB) repository.TodoRepository {
-	return &todoRepository{db}
+func NewTodoRepository(db *storm.DB) TodoRepository {
+	return &TodoRepository{db}
 }
 
-func (t *todoRepository) GetAll([]model.Task, error) {
+func (t *TodoRepository) GetAll([]model.Todo, error) {
 	panic("implement me")
 }
 
 func Create(message string) error {
-	t := todo{string(rune(time.Now().Unix())), message, time.Now().Format("2006-01-02 03:04:05")}
+	t := model.Todo{string(rune(time.Now().Unix())), message, time.Now().Format("2006-01-02 03:04:05")}
 	value, err := json.Marshal(t)
 	if err != nil {
-		insert(bucket, []byte(t.Id), value)
+		fmt.Println(value)
+		//insert(bucket, []byte(t.Id), value)
 	}
 	return err
 }
