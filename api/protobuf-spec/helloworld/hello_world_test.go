41eff437293a83d9a5219f0f1b5f9798de00e22c
Liyang <ismeade99@sina.com>
2022-11-04 18:49:53 +0800

    feat:test code

---
+++ api/protobuf-spec/helloworld/hello_world_test.go
@@ -0,0 +1,72 @@
+package helloworld
+
+import (
+	"context"
+	"log"
+	"net"
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type server struct {
+	UnimplementedGreeterServer
+}
+
+func NewServer() *server {
+	return &server{}
+}
+
+func (s *server) SayHello(ctx context.Context, in *HelloRequest) (*HelloReply, error) {
+	return &HelloReply{Message: in.Name + " world"}, nil
+}
+
+func TestMain(m *testing.M) {
+	// Create a listener on TCP port
+	lis, err := net.Listen("tcp", ":8091")
+	if err != nil {
+		log.Fatalln("Failed to listen:", err)
+	}
+
+	// 创建一个gRPC server对象
+	s := grpc.NewServer()
+	// 注册Greeter service到server
+	RegisterGreeterServer(s, &server{})
+
+	// gRPC-Gateway mux
+	gwmux := runtime.NewServeMux()
+	dops := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	err = RegisterGreeterHandlerFromEndpoint(context.Background(), gwmux, "127.0.0.1:8091", dops)
+	if err != nil {
+		log.Fatalln("Failed to register gwmux:", err)
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/", gwmux)
+
+	// 定义HTTP server配置
+	gwServer := &http.Server{
+		Addr:    "127.0.0.1:8091",
+		Handler: grpcHandlerFunc(s, mux), // 请求的统一入口
+	}
+	log.Println("Serving on http://127.0.0.1:8091")
+	log.Fatalln(gwServer.Serve(lis)) // 启动HTTP服务
+
+}
+
+// grpcHandlerFunc 将gRPC请求和HTTP请求分别调用不同的handler处理
+func grpcHandlerFunc(grpcServer *grpc.Server, otherHandler http.Handler) http.Handler {
+	return h2c.NewHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
+			grpcServer.ServeHTTP(w, r)
+		} else {
+			otherHandler.ServeHTTP(w, r)
+		}
+	}), &http2.Server{})
+}
