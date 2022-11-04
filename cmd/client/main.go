c42fc0d5052ec80e3ef453a7edb11e392eeafc6f
Liyang <ismeade99@sina.com>
2022-11-04 19:44:44 +0800

    feat:test

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
 	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 
-	helloworldpb "go-todolist/api/protobuf-spec/helloworld"
+	"go-todolist/api/protobuf-spec/task"
 )
 
 // hello_client
@@ -19,26 +20,27 @@ const (
 )
 
 var (
-	addr = flag.String("addr", "127.0.0.1:8091", "the address to connect to")
+	addr = flag.String("addr", "127.0.0.1:8972", "the address to connect to")
 	name = flag.String("name", defaultName, "Name to greet")
 )
 
 func main() {
 	flag.Parse()
+	fmt.Printf("非命令行参数切片：%v\n", flag.Args())
 	// 连接到server端，此处禁用安全传输
 	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
-	c := helloworldpb.NewGreeterClient(conn)
+	c := task.NewTaskServiceClient(conn)
 
 	// 执行RPC调用并打印收到的响应数据
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	r, err := c.SayHello(ctx, &helloworldpb.HelloRequest{Name: *name})
+	r, err := c.ListTask(ctx, &task.ListTaskRequest{UserId: 1})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
-	log.Printf("Greeting: %s", r.GetMessage())
+	log.Printf("Greeting: %d", r.Count)
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,72 +2,40 @@ package main
 
 import (
 	"context"
-	"log"
+	"fmt"
 	"net"
-	"net/http"
-	"strings"
 
-	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
-	"golang.org/x/net/http2"
-	"golang.org/x/net/http2/h2c"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 
-	helloworldpb "go-todolist/api/protobuf-spec/helloworld"
+	"go-todolist/api/protobuf-spec/task"
 )
 
-type server struct {
-	helloworldpb.UnimplementedGreeterServer
+type TaskService struct {
+	task.UnimplementedTaskServiceServer
 }
 
-func NewServer() *server {
-	return &server{}
+func NewServer() *TaskService {
+	return &TaskService{}
 }
 
-func (s *server) SayHello(ctx context.Context, in *helloworldpb.HelloRequest) (*helloworldpb.HelloReply, error) {
-	return &helloworldpb.HelloReply{Message: in.Name + " world"}, nil
+//
+func (s *TaskService) ListTask(ctx context.Context, in *task.ListTaskRequest) (*task.ListTaskReply, error) {
+	return &task.ListTaskReply{Count: 10}, nil
 }
 
 func main() {
-	// Create a listener on TCP port
-	lis, err := net.Listen("tcp", ":8091")
+	// 监听本地的8972端口
+	lis, err := net.Listen("tcp", ":8972")
 	if err != nil {
-		log.Fatalln("Failed to listen:", err)
+		fmt.Printf("failed to listen: %v", err)
+		return
 	}
-
-	// 创建一个gRPC server对象
-	s := grpc.NewServer()
-	// 注册Greeter service到server
-	helloworldpb.RegisterGreeterServer(s, &server{})
-
-	// gRPC-Gateway mux
-	gwmux := runtime.NewServeMux()
-	dops := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
-	err = helloworldpb.RegisterGreeterHandlerFromEndpoint(context.Background(), gwmux, "127.0.0.1:8091", dops)
+	s := grpc.NewServer()                             // 创建gRPC服务器
+	task.RegisterTaskServiceServer(s, &TaskService{}) // 在gRPC服务端注册服务
+	// 启动服务
+	err = s.Serve(lis)
 	if err != nil {
-		log.Fatalln("Failed to register gwmux:", err)
+		fmt.Printf("failed to serve: %v", err)
+		return
 	}
-
-	mux := http.NewServeMux()
-	mux.Handle("/", gwmux)
-
-	// 定义HTTP server配置
-	gwServer := &http.Server{
-		Addr:    "127.0.0.1:8091",
-		Handler: grpcHandlerFunc(s, mux), // 请求的统一入口
-	}
-	log.Println("Serving on http://127.0.0.1:8091")
-	log.Fatalln(gwServer.Serve(lis)) // 启动HTTP服务
-
-}
-
-// grpcHandlerFunc 将gRPC请求和HTTP请求分别调用不同的handler处理
-func grpcHandlerFunc(grpcServer *grpc.Server, otherHandler http.Handler) http.Handler {
-	return h2c.NewHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
-			grpcServer.ServeHTTP(w, r)
-		} else {
-			otherHandler.ServeHTTP(w, r)
-		}
-	}), &http2.Server{})
 }
